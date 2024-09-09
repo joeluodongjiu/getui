b58824a50c7d335b98238763536ae70016b1b68b
luodongjiu <luodongjiu@37.com>
2024-09-09 17:49:03 +0800

    fix(publics): 修复options json 结构

--- publics/public_struct.go
+++ publics/public_struct.go
@@ -163,34 +163,6 @@ type Notification struct {
 	 * 举例：角标数字配置1，应用之前角标数为2，发送此角标消息后，应用角标数显示为3。
 	 * 客户端SDK最低要求 2.14.0.0
 	 */
-
-	// options为push_channel厂商通道中安卓专有
-	Options OptionsFix `json:"options,omitempty"` // 第三方厂商通知扩展内容
-
-}
-
-type OptionsFix map[string]map[string]interface{}
-
-type Options struct {
-	Constraint string `json:"constraint,omitempty"` // 非必须，扩展内容对应厂商通道设置如：HW,MZ,...
-	Key        string `json:"key,omitempty"`
-	/*
-	 * Key,必须，默认值：无
-	 * 厂商内容扩展字段,单个厂商特有字段
-	 * key目前支持的所有字段：
-	 * hw角标设置：badgeAddNum
-	 * badgeClass要设置hw角标，这两个字段需要配合使用 ，hw的icon，op私信 channel，op的消息去重 app_meaasge_id
-	 * vv的消息分类classification， 0 代表运营消息，1代表系统消息，不填默认为0
-	 * xm的channel:目前只有op和xm支持
-	 */
-	Value string `json:"value,omitempty"`
-	/*
-	 * value,必须，默认值：无
-	 * value的设置根据key值决定。例如
-	 * hw角标设置：key设为badgeAddNum，value：1（原来的角标值+1）
-	 * key设为badgeClass，value：请写入角标设置的应用类名）
-	 * key设为icon，value：请写⼊对应图标地址
-	 */
 }
 
 // 撤回消息时使用，与notification、transmission三选一，都填写时报错
@@ -263,4 +235,30 @@ type AndroidChannel struct {
 type Ups struct {
 	Notification *Notification `json:"notification"` // 非必须,通知消息内容，与transmission 二选一，两个都填写时报错
 	TransMission string        `json:"transmission"` // 非必须,透传消息内容，与notification 二选一，两个都填写时报错，长度 ≤ 3072
+	// options为push_channel厂商通道中安卓专有
+	Options OptionsFix `json:"options,omitempty"` // 第三方厂商通知扩展内容
+}
+
+type OptionsFix map[string]map[string]interface{}
+
+type Options struct {
+	Constraint string `json:"constraint,omitempty"` // 非必须，扩展内容对应厂商通道设置如：HW,MZ,...
+	Key        string `json:"key,omitempty"`
+	/*
+	 * Key,必须，默认值：无
+	 * 厂商内容扩展字段,单个厂商特有字段
+	 * key目前支持的所有字段：
+	 * hw角标设置：badgeAddNum
+	 * badgeClass要设置hw角标，这两个字段需要配合使用 ，hw的icon，op私信 channel，op的消息去重 app_meaasge_id
+	 * vv的消息分类classification， 0 代表运营消息，1代表系统消息，不填默认为0
+	 * xm的channel:目前只有op和xm支持
+	 */
+	Value string `json:"value,omitempty"`
+	/*
+	 * value,必须，默认值：无
+	 * value的设置根据key值决定。例如
+	 * hw角标设置：key设为badgeAddNum，value：1（原来的角标值+1）
+	 * key设为badgeClass，value：请写入角标设置的应用类名）
+	 * key设为icon，value：请写⼊对应图标地址
+	 */
 }
