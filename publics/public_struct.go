1b1a4b1b5a90dcb62cdbd3a50d01283de4d1c684
luodongjiu <luodongjiu@37.com>
2025-04-30 14:42:49 +0800

    feat(main): 鸿蒙系统新增回调通知

--- publics/public_struct.go
+++ publics/public_struct.go
@@ -157,7 +157,7 @@ type Notification struct {
 	 * 示例：intent:#Intent;component=你的包名/你要打开的 activity 全路径;S.parm1=value1;S.parm2=value2;end
 	 * 如何生成：https://github.com/GetuiLaboratory/getui-pushapi-java-demo/blob/master/intent%E7%94%9F%E6%88%90%E5%8F%82%E8%80%83%E7%A4%BA%E4%BE%8B.md
 	 */
-	Want string `json:"want_intent,omitempty"`
+	Want string `json:"want,omitempty"`
 	// 鸿蒙系统当，click_type为intent时必填
 	Url         string `json:"url,omitempty"`       // click_type为url时必填,点击通知打开链接，长度 ≤ 1024
 	PayLoad     string `json:"payload,omitempty"`   // click_type为payload/payload_custom时必填,点击通知加自定义消息，长度 ≤ 3072
